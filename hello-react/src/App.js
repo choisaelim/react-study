3e144f239185e5823e73bf1d9a30bce8f0039497
choisaelim <choisaelim@gmail.com>
2021-01-01 00:10:20 +0900

    state와 button onclick

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import MyComponent from './MyComponent';
+import Counter from './Counter';
 
 const App = () => {
-  return <MyComponent name="react" favoriteNumber = "3">
-        react</MyComponent>;
+  // return <MyComponent name="react" favoriteNumber = "3">
+  //       react</MyComponent>;
+  return <Counter/>;
 };
 
 export default App;
