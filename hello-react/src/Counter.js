621b32c1409476e461b7110fbc3a18ddfa944e55
choisaelim <choisaelim@gmail.com>
2021-01-01 00:18:03 +0900

    call back 추가

--- hello-react/src/Counter.js
+++ hello-react/src/Counter.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    constructor(props){
-        super(props);
+    // constructor(props){
+    //     super(props);
 
-        this.state = {
-            number: 0
-        };
-    }
+    //     this.state = {
+    //         number: 0,
+    //         fixedNumber: 0
+    //     };
+    // }
+    state = {
+        number : 0,
+        fixedNumber : 0
+    };
     render() {
-        const {number} = this.state;
+        const {number, fixedNumber} = this.state; //state 조회
         return (
             <div>
                 <h1>{number}</h1>
+                <h1>fixed number : {fixedNumber}</h1>
                 <button onClick = {() => {
-                    this.setState({number: number + 1});
+                    // this.setState({number: number + 1});
+                    this.setState( (prevState, props) => {
+                        return {number: prevState.number + 1};
+                    },
+                    //callback
+                    () => {
+                        console.log(this.state);
+                    }
+                    );
                 }}>+1</button>
                 
             </div>
