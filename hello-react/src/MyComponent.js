dc130776b78efa3d31819b4e25d3fb4b0119fda2
choisaelim <choisaelim@gmail.com>
2020-12-31 23:53:16 +0900

    props 연습

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
 //export 연습
-const MyComponent = () => {
-    return <div> New Component </div>;
+const MyComponent = props => {
+    //props 추가
+    const {name, children} = props;
+    return (
+        <div> My name is {name} <br/>
+            children is {children} 
+        </div>
+    );
+}
+
+MyComponent.defaultProps = {
+    name : 'default name'
 }
 
 export default MyComponent;
