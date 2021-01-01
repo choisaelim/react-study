51c676144fd2086d06fe6ec3a1db029e20283a28
choisaelim <choisaelim@gmail.com>
2021-01-01 23:20:01 +0900

    useState

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import MyComponent from './MyComponent';
 import Counter from './Counter';
+import Say from './Say';
 
 const App = () => {
   // return <MyComponent name="react" favoriteNumber = "3">
   //       react</MyComponent>;
-  return <Counter/>;
+  return <Say/>;
 };
 
 export default App;
