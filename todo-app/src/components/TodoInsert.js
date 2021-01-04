3bc5c793a22c2ca4502f9bde76c613c78776c68a
SL.Choi <SL.Choi@tsb.co.kr>
2021-01-04 23:27:19 +0900

    Insert에 form과 value 추가

--- todo-app/src/components/TodoInsert.js
+++ todo-app/src/components/TodoInsert.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useState, useCallback} from 'react';
 import {MdAdd} from 'react-icons/md';
 
 const TodoInsert = () => {
+    const [value, setValue] = useState('');
+
+
     return (
-        <div>
-            
-        </div>
+        <form className="TodoInsert" >
+            <input
+                placeholder="할 일을 입력하세요"
+                value={value}
+                
+            />
+            <button type="submit">
+                <MdAdd />
+            </button>
+        </form>
     );
 };
 
